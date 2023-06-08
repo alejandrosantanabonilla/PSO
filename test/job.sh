e4233fcab11dac1030b9e5a2f969109bf8d1a315
Alejandro Santana-Bonilla <62341142+alejandrosantanabonilla@users.noreply.github.com>
2023-06-08 14:42:42 +0100

    Create job.sh

---
+++ test/job.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+#$ -l h_rt=48:00:00
+#$ -l mem=4.5G
+#$ -pe mpi 40
+#$ -N pso_calculation
+#$ -A KCL_Cedric 
+#$ -P Gold
+#$ -cwd 
+
+module purge
+module load gcc-libs/4.9.2
+module load compilers/intel/2017/update1
+module load mpi/intel/2017/update1/intel
+module load vasp/5.4.4-18apr2017/intel-2017-update1
+module load openblas/0.3.7-serial/gnu-4.9.2
+module load python3/3.8
+
+
+source $HOME//bin/activate
+export OMP_NUM_THREADS=1
+
+start=`date +%s`
+python test_1.py  > output.out
+end=`date +%s`
+echo Execution time was `expr $end - $start` seconds.
